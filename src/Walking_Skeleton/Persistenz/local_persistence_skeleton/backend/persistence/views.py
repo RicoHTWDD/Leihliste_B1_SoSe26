9c7c9c572ab8de47e44edccbac3231ee9c22442b
Yeelius <julius.neubert777@gmail.com>
2026-06-04 16:10:07 +0200

    added: function that automatically changes the notes.json file in data

--- src/Walking_Skeleton/Persistenz/local_persistence_skeleton/backend/persistence/views.py
+++ src/Walking_Skeleton/Persistenz/local_persistence_skeleton/backend/persistence/views.py
@@ -6,3 +6,8 @@ from .export import export_notes_to_json
 class NoteListCreateView(generics.ListCreateAPIView):
     queryset = Note.objects.order_by("-created_at")
     serializer_class = NoteSerializer
+
+    def perform_create(self, serializer):
+        note = serializer.save()
+        export_notes_to_json()
+        return note
